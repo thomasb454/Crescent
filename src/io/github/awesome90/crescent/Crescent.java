b4382bacacb6cf996d6548f69948754e101be8f9
awesome90 <awesome999888@outlook.com>
2017-02-21 20:07:12 +0000

    Removed unneeded imports.

--- src/io/github/awesome90/crescent/Crescent.java
+++ src/io/github/awesome90/crescent/Crescent.java
@@ -1,7 +1,6 @@
 package io.github.awesome90.crescent;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
