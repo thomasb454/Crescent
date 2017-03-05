0c9fcd5376cccea21a13465477f89f722499b4dd
awesome90 <awesome999888@outlook.com>
2017-03-05 19:34:50 +0000

    Changed KillauraD.

--- src/io/github/awesome90/crescent/detection/checks/damage/killaura/KillauraD.java
+++ src/io/github/awesome90/crescent/detection/checks/damage/killaura/KillauraD.java
@@ -31,19 +31,35 @@ public class KillauraD extends CheckVersion {
 			LivingEntity le = (LivingEntity) edbe.getEntity();
 
 			final Location playerEye = player.getEyeLocation();
-			final Location target = le.getLocation();
+			final Location target = le.getLocation().clone().add(0, 1, 0);
 
-			final double distance = getExpectedDistance(playerEye, target);
-			final double expectedDistance = Math
-					.abs(Math.abs(playerEye.getY() - target.getY()) / Math.sin(Math.abs(playerEye.getPitch())));
+			final double distance = round(getExpectedDistance(playerEye, target));
+			final double expectedDistance = round(
+					Math.abs(Math.abs(playerEye.getY() - target.getY()) / Math.sin(Math.abs(playerEye.getPitch()))));
 
 			Bukkit.broadcastMessage("expected: " + expectedDistance + " actual: " + distance);
 		}
 	}
 
+	/**
+	 * @param from
+	 *            The location from where we want to start measuring.
+	 * @param to
+	 *            The location we want to measure to.
+	 * @return The distance.
+	 */
 	private double getExpectedDistance(Location from, Location to) {
-		return Math.sqrt(Math.pow(from.getX() - to.getX(), 2.0) + Math.pow(from.getY() - to.getY(), 2.0)
-				+ Math.pow(from.getZ() - to.getZ(), 2.0));
+		return Math.pow(from.getX() - to.getX(), 2.0) + Math.pow(from.getY() - to.getY(), 2.0)
+				+ Math.pow(from.getZ() - to.getZ(), 2.0);
+	}
+
+	/**
+	 * @param value
+	 *            The value to round.
+	 * @return A value rounded to the nearest 0.5.
+	 */
+	private double round(double value) {
+		return Math.round(value) + 0.5;
 	}
 
 	@Override
